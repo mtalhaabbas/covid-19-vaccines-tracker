c967d7b6b42da02b46958d8cec60f73c20407c60
Muhammad Talha Abbas <70336405+mtalhaabbas@users.noreply.github.com>
2021-08-30 14:44:48 +0500

    Updates

---
+++ precache-manifest.3a59f5485e643af49bccab81dcb57fce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2660bb31ef575692974bd30d7a76061b",
+    "url": "/covid-19-vaccines-tracker/index.html"
+  },
+  {
+    "revision": "9c7bfe0e87fe7f64d73a",
+    "url": "/covid-19-vaccines-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "052ea88d473b5e5adef7",
+    "url": "/covid-19-vaccines-tracker/static/css/main.3077fb0b.chunk.css"
+  },
+  {
+    "revision": "9c7bfe0e87fe7f64d73a",
+    "url": "/covid-19-vaccines-tracker/static/js/2.f86732e8.chunk.js"
+  },
+  {
+    "revision": "9922ea9069e02abaf7aeceb5abadc3c8",
+    "url": "/covid-19-vaccines-tracker/static/js/2.f86732e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052ea88d473b5e5adef7",
+    "url": "/covid-19-vaccines-tracker/static/js/main.8817901b.chunk.js"
+  },
+  {
+    "revision": "c6b2a88c6608594e021f",
+    "url": "/covid-19-vaccines-tracker/static/js/runtime-main.7d3597de.js"
+  }
+]);
